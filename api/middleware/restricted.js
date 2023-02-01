b9a32d155cbad2fe2d254c75d6a1f522d1bb06cb
Morgan Frotten <mmfrotten@gmail.com>
2023-02-01 13:13:49 -0500

    token check

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -4,15 +4,16 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (!token) {
-     return res.status(401).json({message: "token required"})
-  }
-  jwt.verify(token, 'the fish flies at night', (err, ) => {
+     res.status(401).json({message: "token required"})
+  } else {
+  jwt.verify(token, 'the fish flies at night', (err, decodedToken) => {
     if (err) {
       res.json(401).json({message: "token invalid"})
    } else {
+      req.decodedToken = decodedToken
       next()
     }
-    })
+  })
   
  
   /*
@@ -26,4 +27,4 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
-};
+}};
